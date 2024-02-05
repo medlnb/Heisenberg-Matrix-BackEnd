fb88526b9b7aea32b652cf8188531f7f02fac11c
TWSU <mohamedlanabi0@gmail.com>
2024-02-05 21:51:45 +0100

    Update check matrixtask fun

--- Controllers/MatrixNotesController.js
+++ Controllers/MatrixNotesController.js
@@ -1,58 +1,55 @@
-const mongoose = require("mongoose")
-const MatrixNote = require('../Models/MatrixNoteModel')
+const mongoose = require("mongoose");
+const MatrixNote = require("../Models/MatrixNoteModel");
 
 const getMatrixTask = async (req, res) => {
   try {
-    const email = req.user.email
-    const Notes = await MatrixNote.find({email})
-    res.status(201).json(Notes)
+    const email = req.user.email;
+    const Notes = await MatrixNote.find({ email });
+    res.status(201).json(Notes);
   } catch (error) {
-    console.log(error)
-    res.status(501).json({error:"error getting the notes"})
+    console.log(error);
+    res.status(501).json({ error: "error getting the notes" });
   }
-}
+};
 
 const addMatrixTask = async (req, res) => {
-   
-  try {  
-    const email = req.user.email
-    const note = await MatrixNote.create({ ...req.body,email,isDone:false })
-    res.status(201).json(note)
+  try {
+    const email = req.user.email;
+    const note = await MatrixNote.create({ ...req.body, email, isDone: false });
+    res.status(201).json(note);
   } catch (error) {
-    console.log(error)
-    res.status(501).json({error:error})
+    console.log(error);
+    res.status(501).json({ error: error });
   }
-}
+};
 const deleteMatrixTask = async (req, res) => {
   if (!mongoose.Types.ObjectId.isValid(req.params.id))
-    return res.status(401).json({ error: "Wrong id form" })
-  
+    return res.status(401).json({ error: "Wrong id form" });
+
   try {
-    const note = await MatrixNote.findOneAndDelete({ _id: req.params.id })
-    res.status(200).json(note)
+    const note = await MatrixNote.findOneAndDelete({ _id: req.params.id });
+    res.status(200).json(note);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
   }
-  catch (error) {
-    console.log(error)
-    res.status(500).json(error)
-}
-  
-}
+};
 
 const updateMatrixTask = async (req, res) => {
-    if (!mongoose.Types.ObjectId.isValid(req.params.id))
-      return res.status(401).json({ error: "Wrong id form" })
-    
-    try {
-      const note = await MatrixNote.findByIdAndUpdate({ _id: req.params.id }, { ...req.body })
-      res.status(201).json(note)
-    } catch (error) {
-      console.log(error)
-    res.status(404).json(error)
-    }
-}
+  if (!mongoose.Types.ObjectId.isValid(req.params.id))
+    return res.status(401).json({ error: "Wrong id form" });
+
+  const note = await MatrixNote.findOne({ _id: req.params.id });
+  note.isDone = !note.isDone;
+
+  const updatedNote = await MatrixNote.save();
+
+  if (!updatedNote) return res.status(404).json({ error: "Note not found" });
+  res.status(201).json(note);
+};
 module.exports = {
   getMatrixTask,
   addMatrixTask,
   deleteMatrixTask,
-  updateMatrixTask
-}
+  updateMatrixTask,
+};

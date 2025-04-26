62d084d037521c863c396d33236a6e653a1708e9
medlnb <mohamedlanabi0@gmail.com>
2025-04-26 18:12:54 +0100

    first commit

--- index.js
+++ index.js
@@ -1,27 +1,20 @@
-const express = require("express")
-const cors = require("cors")
-const mongoose = require("mongoose")
-const MatrixTaskrouter = require("./Routers/MatrixNote")
-const Authrouter = require('./Routers/User')
-const Tasksrouter = require("./Routers/Tasks")
-const Notesrouter = require("./Routers/Notes")
-require("dotenv").config()
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const RoomRouter = require("./Routers/Room");
+const ImageRouter = require("./Routers/image");
+require("dotenv").config();
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
+app.use("/api/room", RoomRouter);
+app.use("/api/image", ImageRouter);
 
-app.use("/api/notes", Notesrouter)
-app.use("/api/matrixTask", MatrixTaskrouter)
-app.use("/api/tasks", Tasksrouter)
-app.use("/api/user", Authrouter)
-
-
-mongoose.connect(process.env.MONGO_URI)
-.then(
+mongoose.connect(process.env.MONGO_URI).then(
   app.listen(process.env.PORT, () => {
-    console.log(`connected to db & listening to ${process.env.PORT}`)
-})  )
-
+    console.log(`connected to db & listening to ${process.env.PORT}`);
+  })
+);
